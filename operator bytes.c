d5e508735d015b0002dc7a21e62300664d0565c0
lithikkumar <kumarlithik534@gmail.com>
2026-02-18 15:41:30 +0530

    Add files via upload

---
+++ operator bytes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	printf("Size of int: %lu bytes\n",sizeof(int));
+	printf("size of char: %lu bytes\n",sizeof(char));
+	printf("size of float: %lu bytes\n",sizeof(float));
+	printf("size of double: %lu bytes\n",sizeof(double));
+	printf("size of short: %lu bytes\n",sizeof(short));
+	printf("size of long: %lu bytes\n",sizeof(long));
+	printf("size of long long: %lu bytes\n",sizeof(long long));
+	return 0;
+	
+}
+
